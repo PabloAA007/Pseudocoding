66c242cb61860349a1d1ef3164f3015a99cd506e
Pablo Aray Acevedo <123980866+PabloAA007@users.noreply.github.com>
2023-05-12 15:48:45 -0400

    Add files via upload

---
+++ conditionally_yours_part_1.py
@@ -0,0 +1,30 @@
+"""
+Conditionally Yours
+
+Pseudocode:
+1. Declare float variable for original_price.
+
+2. Declare float variable for current_price.
+
+3. Calculate the difference between current_price and original_price and assign it to a variable increase.
+    increase = current_price - original_price
+
+4. Calculate percent increase using the formula:
+    percent_increase = (increase / original_price) * 100
+
+5. Declare float variable for threshold, set it to 20 (assuming this represents 20% change in stock price).
+
+6. Implement if-else logic:
+
+    IF percent_increase is greater than or equal to threshold:
+        Print "Buy the Netflix stock."
+    
+    ELSE IF percent_increase is less than or equal to -threshold:
+        Print "Sell the Netflix stock."
+    
+    ELSE:
+        Print "Hold the Netflix stock, no significant change in price."
+
+
+
+"""
